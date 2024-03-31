0969b8aa3129aada5c773f99919188de790d1852
suimisama <471899040@qq.com>
2024-03-31 13:12:35 +0800

    refactor: refactor App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,33 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './index.css';
 import Box from './ui/Box';
 import Button from './ui/Button';
-function getRandomColor() {
-  let letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
+import { getRandomColor } from './utilities/helpers';
+
 function App() {
-  let [randomColors, setRandomColors] = useState([]);
-  function updateColors() {
+  let [randomColors, setRandomColors] = useState(createColors());
+  function createColors() {
     let colors = [];
-    for (let i = 0; i < 25; i++) {
+    for (let i = 0; i < 36 * 3; i++) {
       const color = getRandomColor();
       colors.push(color);
     }
+    return colors;
+  }
+  function handleUpdateColors() {
+    const colors = createColors();
     setRandomColors(colors);
   }
-  useEffect(function () {
-    updateColors();
-  }, []);
+
   return (
     <div className="h-screen">
       <div className="flex flex-col justify-center items-center h-full p-4 flex-wrap">
-        <div className="flex justify-center items-center gap-4 mb-6">
+        <div className="flex items-center gap-4 mb-6 translate-all flex-wrap w-2/3">
           {randomColors.map((color, index) => (
             <Box color={color} key={index} />
           ))}
         </div>
-        <Button onClick={updateColors}>随机颜色</Button>
+        <Button onClick={handleUpdateColors}>随机颜色</Button>
       </div>
     </div>
   );
